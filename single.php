35e732657b7b4618f40673e6f6ec939c6881ef34
sysreqx <sysreqx@gmail.com>
2019-01-07 16:34:07 +0600

    single post template for displaying blog posts

---
+++ single.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The template for displaying the posts
+ *
+ * @package CustomTheme
+ * @since Custom Theme 1.0
+ */
+?>
+
+<?php get_header(); ?>
+
+<div class="container">
+	<?php if (have_posts() ) : ?>
+		<?php while (have_posts() ) : the_post(); ?>
+			<h2>A single blog post</h2>
+
+			<?php the_title(); ?>
+			<?php the_content(); ?>
+		<?php endwhile; ?>
+	<?php endif; ?>
+
+	<?php get_sidebar() ?>
+</div>
+
+<?php get_footer(); ?>
