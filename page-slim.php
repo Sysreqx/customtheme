ce5f0cfcc3b4780cc38fb4411cb471cc8c61bdc5
sysreqx <sysreqx@gmail.com>
2019-01-07 16:51:47 +0600

    blog layout

--- page-slim.php
+++ page-slim.php
@@ -11,7 +11,7 @@
 
 <div class="container">
 	<div class="row">
-		<div class="col-md-6 col-md-offset-3">
+		<div class="col-md-6 .offset-md-3">
 			<?php if (have_posts() ) : ?>
 				<?php while (have_posts() ) : the_post(); ?>
 
